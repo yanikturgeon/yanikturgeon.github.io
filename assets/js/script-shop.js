4e56d1fd8a616dfa62bffbff0b2643d065e03d2e
yanikturgeon <yanikturgeon@gmail.com>
2021-07-21 08:37:44 +0800

    2nd commit

---
+++ assets/js/script-shop.js
@@ -0,0 +1,122 @@
+$(document).ready(function() {
+    var bigimage = $("#big");
+    var thumbs = $("#thumbs");
+    //var totalslides = 10;
+    var syncedSecondary = true;
+
+    bigimage
+        .owlCarousel({
+            items: 1,
+            nav: true,
+            dots: false,
+            responsiveRefreshRate: 200,
+            navText: [
+                '<span>‹</span>',
+                '<span>›</span>'
+            ]
+        })
+        .on("changed.owl.carousel", syncPosition);
+
+    thumbs
+        .on("initialized.owl.carousel", function() {
+            thumbs
+                .find(".owl-item")
+                .eq(0)
+                .addClass("current");
+        })
+        .owlCarousel({
+            items: 4,
+            dots: true,
+            nav: true,
+            slideBy: 4,
+            responsiveRefreshRate: 100
+        })
+        .on("changed.owl.carousel", syncPosition2);
+
+    function syncPosition(el) {
+        //if loop is set to false, then you have to uncomment the next line
+        var current = el.item.index;
+
+        //to disable loop, comment this block
+        // var count = el.item.count - 1;
+        // var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+
+        // if (current < 0) {
+        //     current = count;
+        // }
+        // if (current > count) {
+        //     current = 0;
+        // }
+        //to this
+        thumbs
+            .find(".owl-item")
+            .removeClass("current")
+            .eq(current)
+            .addClass("current");
+        var onscreen = thumbs.find(".owl-item.active").length - 1;
+        var start = thumbs
+            .find(".owl-item.active")
+            .first()
+            .index();
+        var end = thumbs
+            .find(".owl-item.active")
+            .last()
+            .index();
+
+        if (current > end) {
+            thumbs.data("owl.carousel").to(current, 100, true);
+        }
+        if (current < start) {
+            thumbs.data("owl.carousel").to(current - onscreen, 100, true);
+        }
+    }
+
+    function syncPosition2(el) {
+        if (syncedSecondary) {
+            var number = el.item.index;
+            bigimage.data("owl.carousel").to(number, 100, true);
+        }
+    }
+
+    thumbs.on("click", ".owl-item", function(e) {
+        e.preventDefault();
+        var number = $(this).index();
+        bigimage.data("owl.carousel").to(number, 300, true);
+    });
+    $('.owl-related').owlCarousel({
+        loop: true,
+        autoplay: false,
+        margin: 10,
+        nav: true,
+        dotsEach: 4,
+        center: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+                dots: false
+            },
+            576: {
+                items: 3,
+                nav: true,
+                dots: false
+            },
+            768: {
+                items: 3,
+                nav: true,
+            },
+            992: {
+                items: 5,
+                nav: true,
+            },
+            1200: {
+                items: 5,
+                nav: true
+            }
+        }
+    });
+});
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
---
+++ assets/js/script.js
@@ -0,0 +1,118 @@
+$(document).ready(function() {
+    $('.owl-hero').owlCarousel({
+        loop: true,
+        autoplay: false,
+        margin: 10,
+        nav: true,
+        dotsEach: 4,
+        center: true,
+        stagePadding: 30,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+                dots: false
+            },
+            576: {
+                items: 3,
+                nav: true,
+                dots: false
+            },
+            768: {
+                items: 3,
+                nav: true,
+            },
+            992: {
+                items: 5,
+                nav: true,
+            },
+            1200: {
+                items: 5,
+                nav: true
+            }
+        }
+    });
+    $('.owl-coins').owlCarousel({
+        items: 10,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        autoplayHoverPause: true,
+        smartSpeed: 100,
+        nav: true,
+        center: true,
+        dots: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 3,
+                nav: true,
+            },
+            576: {
+                items: 5,
+                nav: true
+            },
+            768: {
+                items: 7,
+                nav: true,
+            },
+            992: {
+                items: 7,
+                nav: true,
+            },
+            1200: {
+                items: 9,
+                nav: true
+            }
+        }
+    });
+    size_li = $("#ourCollections div.col").length;
+    // alert($("#ourCollections div.col").length);
+    x = 8;
+    $('#ourCollections div.col:lt(4)').show();
+    $('#loadMore').click(function() {
+        x = (x + 4 <= size_li) ? x + 4 : size_li;
+        $('#ourCollections div.col:lt(' + x + ')').show();
+        $('#showLess').show();
+        if (x == size_li) {
+            $('#loadMore').hide();
+        }
+    });
+    $('#showLess').click(function() {
+        x = (x - 4 < 0) ? 4 : x - 4;
+        $('#ourCollections div.col').not(':lt(' + x + ')').hide();
+        $('#loadMore').show();
+        $('#showLess').show();
+        if (x == 8) {
+            $('#showLess').hide();
+        }
+    });
+    // Set the date we're counting down to
+    var countDownDate = new Date("August 5, 2021 23:59:59").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("timer").innerHTML = days + "<span>天</span>" + hours + "<span>小時</span>" +
+            minutes + "<span>分鐘</span>" + seconds + "<span>秒</span>";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "<span>已到期</span>";
+        }
+    }, 1000);
+});
